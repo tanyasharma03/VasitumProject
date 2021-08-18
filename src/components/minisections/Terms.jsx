73a7c68b632a4d444f4de510b2d9d8ad5b75c1b7
Tanya Sharma <tasu2612@gmail.com>
2021-08-18 13:46:27 +0530

    profile section created

--- src/components/minisections/Terms.jsx
+++ src/components/minisections/Terms.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import MailOutlineOutlinedIcon from '@material-ui/icons/MailOutlineOutlined';
+import AcUnitIcon from '@material-ui/icons/AcUnit';
 
 const Terms = () => {
     return (
         <>
-            <h1>hello</h1>
+            <div className="terms">
+                <span>About </span>
+                <span>Terms & Conditions </span>
+                <span>Our Team </span>
+                <span>Blog </span>
+                <span>Privacy Policy</span>
+                <div style={{display:"flex", marginTop:10, marginBottom:12}}>
+                <MailOutlineOutlinedIcon className="mail"/><p>meet@vasitum.com</p>
+                </div>
+                <hr/>
+                <div style={{display:'flex', marginTop:20, alignItems:'center'}}>
+                <AcUnitIcon className="icon"/>
+                    <h4>Vasitum </h4>
+                    <p style={{color:"#000"}}>Vasitum ©2021</p>
+                </div>
+            </div>
         </>
     )
 }
